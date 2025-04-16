bd37da34ea5c65a44e056b550737ccd339c683ee
Novice-zone <Z18523157968@163.com>
2025-04-16 22:58:02 +0800

    插入排序的实现

---
+++ InsertSort_ShellSort/InsertSort_ShellSort/Sort.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include"Sort.h"
+
+void PrintArray(int* a, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
+
+// 升序
+// 最坏：O(N^2)  逆序
+// 最好：O(N)    顺序有序
+void InsertSort(int* a, int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		int end = i - 1;
+		int tmp = a[i];
+		// 将tmp插入到[0,end]区间中，保持有序
+		while (end >= 0)
+		{
+			if (tmp < a[end])
+			{
+				a[end + 1] = a[end];
+				--end;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		a[end + 1] = tmp;
+	}
+}
---
+++ InsertSort_ShellSort/InsertSort_ShellSort/Sort.h
@@ -0,0 +1,7 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<stdbool.h>
+
+void PrintArray(int* a, int n);
+void InsertSort(int* a, int n);
