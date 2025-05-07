0c99ddd3960c0994d18a341ba4880b53e7025740
Novice-zone <Z18523157968@163.com>
2025-05-07 23:15:38 +0800

    结束摆烂第一天：看了看c++历史和发展，学了一下命名空间的使用

---
+++ cpp_5_7/cpp_5_7/test.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int a = 10;
+
+namespace haha{
+	int a = 100;
+}
+//using namespace haha;
+
+int main() {
+	int a = 1;
+	printf("%d\n", a);
+	
+	printf("%d\n", ::a);
+
+	printf("%d\n", haha::a);
+
+	return 0;
+}
