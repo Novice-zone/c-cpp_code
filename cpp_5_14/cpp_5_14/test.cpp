882e0200ead12a742bcbd0bd4f5b99c2f0393bb6
Novice-zone <Z18523157968@163.com>
2025-05-15 23:55:32 +0800

    c++入门，函数重载和引用（部分）

--- cpp_5_14/cpp_5_14/test.cpp
+++ cpp_5_14/cpp_5_14/test.cpp
@@ -2,27 +2,55 @@
 using namespace std;
 
 //形参个数重载
-
-//形参类型重载
-
-//形参顺序重载
-//void Func(int a, char b) {
-//	cout << "Func(int a,char b)" << endl;
+//void Func(int a) {
+//	cout <<"int a:" << a << endl;
 //}
-////错误重载
-////void Func(int b,char a){
-////	cout << "Func(int b,char a)" << endl;
-////}
-////正确
-//void Func(char b, int a) {
-//	cout << "Func(char b,int a)" << endl;
+//void Func(int a, char b) {
+//	cout << "int a:" << a << '\n' << "char b:" << b << endl;
+//}
 //
+//int  main() {
+//	int a = 1;
+//	char b = 'D';
+//
+//	Func(a);
+//	Func(a, b);
+//
+//	return 0;
+//}
+//形参类型重载
+//void Func(int a) {
+//	cout << "int :" << a << endl;
+//}
+//void Func(double b) {
+//	cout << "double :" << b << endl;
 //}
 //
 //int main() {
-//	int n = 1;
-//	char m = 'm';
-//	Func(n, m);
-//	Func(m, n);
+//	int a = 1;
+//	double b = 1.1;
+//	Func(a);
+//	Func(b);
 //	return 0;
 //}
+//形参顺序重载
+void Func(int a, char b) {
+	cout << "Func(int a,char b)" << endl;
+}
+//错误重载
+//void Func(int b,char a){
+//	cout << "Func(int b,char a)" << endl;
+//}
+//正确
+void Func(char b, int a) {
+	cout << "Func(char b,int a)" << endl;
+
+}
+
+int main() {
+	int n = 1;
+	char m = 'm';
+	Func(n, m);
+	Func(m, n);
+	return 0;
+}
---
+++ cpp_5_16/cpp_5_16/test.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream>
+using namespace std;
+
+//int main() {
+//	int a = 0;
+//	//int& m;
+//
+//	int& b = a;
+//	int& c = b;
+//	int& d = c;
+//
+//	cout << a << endl << b << endl << c << endl << d << endl;
+//
+//	c = 9;
+//	cout << a << endl << b << endl << c << endl << d << endl;
+//
+//	int m = 10;
+//	//&b = m;
+//	b = m;
+//	cout << a << endl << b << endl << c << endl << d << endl;
+//
+//	return 0;
+//}
+
+//引用的好处
+//void Swap(int* a, int* b) {
+//	int tmp = *a;
+//	*a = *b;
+//	*b = tmp;
+//}
+//
+//int main() {
+//	int a = 1;
+//	int b = 10;
+//	cout << "a:" << a << " b:" << b << endl;
+//
+//	Swap(&a, &b);
+//	cout << "a:" << a << " b:" << b << endl;
+//
+//	return 0;
+//}
+void Swap(int& a, int& b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+int main() {
+	int a = 0;
+	int b = 10;
+	cout << "a:" << a << " b:" << b << endl;
+
+	Swap(a, b);
+	cout << "a:" << a << " b:" << b << endl;
+	return 0;
+}
